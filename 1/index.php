ab8acbc185f0c3c6a51e11836feb71d2ce4a8eba
giiithub1-pixel <giiithub1@gmail.com>
2025-10-28 05:59:02 +0530

    Update index.php

--- 1/index.php
+++ 1/index.php
@@ -111,7 +111,7 @@
   <script>
     // Secure Access System
     function generateLink() {
-      fetch('1/generate.php')
+      fetch('generate.php')
         .then(response => response.json())
         .then(data => {
           if (data.success) {
